835b955e6ca8dd7c66dbdb5a0ba0c4409c859d2e
chochojj <csgy114@gmail.com>
2023-07-12 12:19:07 +0900

    test : api 테스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div></div>;
-}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [responseData, setResponseData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("");
+      setResponseData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  return (
+    <div>
+      {responseData ? (
+        <div>
+          <h2>API Response:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default App;
