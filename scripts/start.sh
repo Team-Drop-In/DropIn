83497666314b4ca37e3394605fd7e904ac2f3f02
CHOHYUNHWA <gusghk115@gmail.com>
2023-07-13 01:56:19 +0900

    fix : scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+PROJECT_ROOT="/home/ubuntu/app/server"
+JAR_FILE="$PROJECT_ROOT/dropin-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
