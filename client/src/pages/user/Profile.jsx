e2765dc59e61d2d62ab6ce8bdea4dab9725cebf9
chochojj <csgy114@gmail.com>
2023-08-27 01:30:27 +0900

    design : 프로필 레이아웃

--- client/src/pages/user/Profile.jsx
+++ client/src/pages/user/Profile.jsx
@@ -13,8 +13,26 @@ const Profile = () => {
             </div>
             <span>닉네임</span>
           </Img>
-          <Info></Info>
+          <Info>
+            <div>
+              <Label>작성한 게시글수</Label>
+              <span>5개</span>
+            </div>
+            <div>
+              <Label>작성한 댓글수</Label>
+              <span>5개</span>
+            </div>
+            <div>
+              <Label>좋아요한 박스 목록</Label>
+              <ul>
+                <li>온오프짐</li>
+                <li>짐박스</li>
+                <li>피트니스</li>
+              </ul>
+            </div>
+          </Info>
         </User>
+        <button></button>
       </Contain>
     </Container>
   );
@@ -26,10 +44,19 @@ const Contain = styled(Content)`
   height: 100vh;
   display: flex;
   flex-direction: column;
+
+  h2 {
+    color: white;
+    font-size: 24px;
+    margin-bottom: 50px;
+  }
 `;
 
 const User = styled.section`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 const Img = styled.div`
@@ -37,5 +64,23 @@ const Img = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  object-fit: contain;
+  width: 400px;
+
+  div {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
 `;
-const Info = styled.div``;
+
+const Info = styled.div`
+  width: 400px;
+
+  div {
+    margin-bottom: 15px;
+  }
+`;
+
+const Label = styled.div``;
