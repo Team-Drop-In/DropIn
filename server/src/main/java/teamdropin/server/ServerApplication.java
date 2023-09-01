a77de0119f09222ad3aed1eeafb14cbfa503946f
CHOHYUNHWA <gusghk115@gmail.com>
2023-09-01 16:40:20 +0900

    fix:환경변수 수정

--- server/src/main/java/teamdropin/server/ServerApplication.java
+++ server/src/main/java/teamdropin/server/ServerApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @EnableJpaAuditing
-
 public class ServerApplication {
 
 	public static void main(String[] args) {
