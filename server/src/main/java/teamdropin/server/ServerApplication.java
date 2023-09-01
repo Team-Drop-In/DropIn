bcd67c1383c3c2a8750e4eba8b1ee87428e29795
CHOHYUNHWA <gusghk115@gmail.com>
2023-09-01 16:30:46 +0900

    fix: 스프링 어노테이션 셋팅 수정

--- server/src/main/java/teamdropin/server/ServerApplication.java
+++ server/src/main/java/teamdropin/server/ServerApplication.java
@@ -9,13 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@SpringBootApplication(
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 @EnableJpaAuditing
 
 public class ServerApplication {
