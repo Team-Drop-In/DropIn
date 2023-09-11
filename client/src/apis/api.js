a2e59385d2918218aa05d738b1e0a8657fc7f301
chochojj <csgy114@gmail.com>
2023-09-11 16:07:46 +0900

    feat: 오픈 그래프 설정

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-43-202-64-101.ap-northeast-2.compute.amazonaws.com:8080",
+  baseURL: process.env.REACT_APP_BaseURL,
   headers: {
     "Content-Type": "application/json",
   },
